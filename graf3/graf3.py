69a86baa7b7e09c0d86a1938fbaf34349bb65ae8
Спарки <cparkygreshin@gmail.com>
2022-01-21 23:06:30 +0200

    v

--- graf3/graf3.py
+++ graf3/graf3.py
@@ -8,23 +8,33 @@ log=Tk()
 log.geometry("300x100")
 log.title("Авторизация")
 def append():
-    if psw.get=="" or login.get=="":
+    global login1
+    global psw1
+    Label(text="Придумайте\nЛогин:").grid(row=0,column=0)
+    login1=Entry(width=30)
+    login1.grid(row=0,column=1,columnspan=3)
+    Label(text="Придумайте\nПароль:").grid(row=1,column=0)
+    psw1=Entry(width=30)
+    psw1.grid(row=1,column=1,columnspan=3)
+    Button(text="Выход",command=Exit2).grid(row=2,column=1)
+    Button(text="Подтвердить",command=append).grid(row=2,column=3)
+    if psw1.get=="" or login1.get=="":
         showerror(title="Error",message="Пароль или логин не может быть пустотой!")
     else:
         showinfo(title="OK",message="Пароль принят и добавлен в базу данных!")
-        userr.append(login.get)
-        passww.append(psw.get)
+        userr.append(login1)
+        passww.append(psw1)
 def Reg():
     log.destroy()
     reg=Tk()
     reg.geometry("300x100")
     reg.title("Регистрация")
     Label(text="Придумайте\nЛогин:").grid(row=0,column=0)
-    login=Entry(width=30)
-    login.grid(row=0,column=1,columnspan=3)
+    login1=Entry(width=30)
+    login1.grid(row=0,column=1,columnspan=3)
     Label(text="Придумайте\nПароль:").grid(row=1,column=0)
-    psw=Entry(width=30)
-    psw.grid(row=1,column=1,columnspan=3)
+    psw1=Entry(width=30)
+    psw1.grid(row=1,column=1,columnspan=3)
     Button(text="Выход",command=Exit2).grid(row=2,column=1)
     Button(text="Подтвердить",command=append).grid(row=2,column=3)
     
